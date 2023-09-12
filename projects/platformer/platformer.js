0722f85ba370d2961860679974726f3f91730f09
hermpayton5 <herman.payton@sciacademy.org>
2023-09-12 15:21:57 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,8 +43,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+    createPlatform(350,250,100,50)
+    createPlatform(50,300,100,50)
 
     
     // TODO 2
@@ -61,8 +61,8 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
+    createCannon("bottom", 350, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("", 550, 200); // cannon on left wall, 600px down, shooting once per second
 
 
     /////////////////////////////////////////////////
