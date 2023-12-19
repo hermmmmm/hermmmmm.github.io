9815486e2fb76e71ae4f04f89b98d1378e2b0b39
hermpayton5 <herman.payton@sciacademy.org>
2023-12-19 14:57:40 +0000

    added code back but error, please check

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -38,7 +38,7 @@ var init = function (window) {
         drawCircle()
         drawCircle()
         drawCircle()
-}
+
         
         
 
@@ -106,7 +106,6 @@ var init = function (window) {
 
         app.addUpdateable(window.opspark.game);
         }
-};
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if ((typeof process !== 'undefined') &&
